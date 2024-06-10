d7287dcf6381ccc99409f4a6e40bb44beca4ef69
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2024-06-10 16:22:32 -0600

    update index.js server side

--- server/index.js
+++ server/index.js
@@ -29,10 +29,11 @@ app.use("/api", router);
 
 
 if (process.env.NODE_ENV === "production"){
-  app.use(express.static('client/build'))
+  app.use(express.static(path.join(__dirname,"client", "build")));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 }
 
