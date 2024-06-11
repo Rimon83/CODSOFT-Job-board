07779aa45f370c1862949f0d2db3b54eac868720
Rimon83 <111208690+Rimon83@users.noreply.github.com>
2024-06-10 19:51:24 -0600

    update index.js

--- server/index.js
+++ server/index.js
@@ -32,8 +32,8 @@ if (process.env.NODE_ENV === "production"){
   app.use(express.static(path.join(__dirname, "client", "build")));
 
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client","index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client","build", "index.html"));
 });
 }
 
